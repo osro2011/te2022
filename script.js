253edb3f1567c7a8bb54e9a74d1ffe8c60397fa6
Oscar Rosenlund <osro2011@edu.laroverken.se>
2022-06-04 03:59:02 +0200

    add hamburger

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleTable() {
     table = document.getElementById("table").classList;
     table.contains("hidden") ? table.remove("hidden") : table.add("hidden");
 }
+
+function toggleNav() {
+    nav = document.getElementById("nav").classList;
+    nav.contains("active") ? nav.remove("active") : nav.add("active");
+}
