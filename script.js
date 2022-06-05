1715a76b58526a0abf336dfb1548a4ea785ed48d
lubr2011 <lubr2011@edu.laroverken.se>
2022-06-05 21:07:28 +0200

    fixat hamburgarmeny och små kommentarer

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function toggleTable() {
+//redundant, potentiell tilläggning
+/*function toggleTable() {
     table = document.getElementById("table").classList;
     table.contains("hidden") ? table.remove("hidden") : table.add("hidden");
-}
+}*/
+//redundant, potentiell tilläggning
 
+//togglar nav via hamburgarmeny
 function toggleNav() {
     nav = document.getElementById("nav").classList;
     nav.contains("active") ? nav.remove("active") : nav.add("active");
 }
+//togglar nav via hamburgarmeny
+
+
+//öppnar listan i hamburgarmeny på mobil
+function openList(elem) {
+    var parent = elem.parentNode;
+    var dropDown = parent.lastElementChild;
+    dropDown.classList.toggle('isOpen');
+}
+//öppnar listan i hamburgarmeny på mobil
+
+
+//gör så du måste dubbelklicka för att öppna länk i mobilläge
+const mediaQuery = window.matchMedia('(max-width: 1650px');
+if (mediaQuery.matches) {
+    Array.from(document.getElementsByClassName('main__link')).forEach(
+        function(element) {
+            element.removeAttribute("href");
+            element.setAttribute("onclick", "openList(this)");
+        }
+    );
+}
+//gör så du måste dubbelklicka för att öppna länk i mobilläge
